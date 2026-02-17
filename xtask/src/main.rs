6b1cc548e7229cb607b4c7181bc3617257dbe5ba
Roberto Aloi <robertoaloi@meta.com>
2026-02-17 12:11:45 -0800

    Remove unused xtask graph functionality
    Summary: We never used this functionality. Remove it, so we don't have to port it to Buck.
    
    Reviewed By: alanz, michalmuskala
    
    Differential Revision: D93452028
    
    fbshipit-source-id: ba6700d035df0b3e04e99ae1f74e260f182afd6e

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -12,7 +12,6 @@
 //! `cargo xtask codegen` for code generation.
 
 use std::env;
-use std::fmt;
 use std::path::Path;
 use std::path::PathBuf;
 
@@ -21,15 +20,6 @@ use anyhow::bail;
 use bpaf::Bpaf;
 use bpaf::Parser;
 use bpaf::construct;
-use krates::Builder;
-use krates::Cmd;
-use krates::DepKind;
-use krates::Krates;
-use krates::Node;
-use krates::Scope;
-use krates::cm;
-use krates::petgraph;
-use krates::petgraph::dot::Config;
 use xshell::Shell;
 use xshell::cmd;
 
@@ -44,22 +34,6 @@ fn main() -> Result<()> {
             let mode = codegen::Mode::Overwrite;
             codegen::CodegenCmd { mode }.run()
         }
-        Command::Graph(_) => {
-            match make_crate_graph() {
-                Ok(contents) => {
-                    let sh = Shell::new()?;
-                    let dot_file = project_root().join("docs/crate_graph.dot");
-                    let png_file = project_root().join("docs/crate_graph.png");
-                    sh.write_file(dot_file.clone(), contents)?;
-                    // Convert to png by
-                    // dot -Tpng crate_graph.dot -ocrate_graph.png
-
-                    cmd!(sh, "dot -Tpng {dot_file} -o{png_file}").output()?;
-                    Ok(())
-                }
-                Err(err) => Err(anyhow::anyhow!("{}", err)),
-            }
-        }
         Help() => {
             eprintln!(
                 "\
@@ -70,8 +44,7 @@ USAGE:
     cargo xtask <SUBCOMMAND>
 
 SUBCOMMANDS:
-    codegen
-    graph"
+    codegen"
             );
             Ok(())
         }
@@ -88,7 +61,6 @@ struct Args {
 #[derive(Clone, Debug)]
 enum Command {
     CodeGen(CodeGen),
-    Graph(Graph),
     Help(),
 }
 
@@ -99,20 +71,11 @@ fn command() -> impl Parser<Command> {
         .command("codegen")
         .help("Generate ast from tree-sitter grammar");
 
-    let graph = graph()
-        .map(Command::Graph)
-        .to_options()
-        .command("graph")
-        .help("Generate crate graph");
-
-    construct!([code_gen, graph]).fallback(Help())
+    construct!([code_gen]).fallback(Help())
 }
 
 #[derive(Clone, Debug, Bpaf)]
 struct CodeGen {}
-
-#[derive(Clone, Debug, Bpaf)]
-struct Graph {}
 
 pub fn project_root() -> PathBuf {
     Path::new(
@@ -145,122 +108,4 @@ fn ensure_rustfmt(sh: &Shell) -> Result<()> {
         )
     }
     Ok(())
-}
-
-fn make_crate_graph() -> Result<String, krates::Error> {
-    let cargo_toml = project_root().join("Cargo.toml");
-    let mut cmd = Cmd::new();
-    cmd.manifest_path(cargo_toml);
-    // Enable all features, works for either an entire workspace or a single crate
-    cmd.all_features();
-
-    let mut builder = Builder::new();
-    builder.workspace(true);
-    builder.ignore_kind(DepKind::Normal, Scope::NonWorkspace);
-    builder.ignore_kind(DepKind::Build, Scope::All);
-    builder.ignore_kind(DepKind::Dev, Scope::All);
-    builder.exclude(
-        [
-            "always-assert:0.1.2",
-            "anyhow:1.0.40",
-            "anymap:0.12.1",
-            "beam_file:0.2.4",
-            "byteorder:1.4.3",
-            "cov-mark:2.0.0-pre.1",
-            "crossbeam-channel:0.5.1",
-            "dashmap:4.0.2",
-            "dissimilar:1.0.2",
-            "drop_bomb:0.1.5",
-            "either:1.6.1",
-            "env_logger:0.8.3",
-            "fxhash:0.2.1",
-            "indexmap:1.6.2",
-            "indicatif:0.16.2",
-            "itertools:0.10.1",
-            "krates:0.9.0",
-            "la-arena:0.2.1",
-            "lazy_static:1.4.0",
-            "libflate:1.1.0",
-            "limit:0.0.0",
-            "log:0.4.14",
-            "lsp-server:0.5.0",
-            "lsp-types:0.89.0",
-            "nonempty:0.6.0",
-            "num:0.4.0",
-            "num-bigint:0.4.0",
-            "num-derive:0.3.3",
-            "num-traits:0.2.14",
-            "once_cell:1.8.0",
-            "parking_lot:0.11.1",
-            "paths:0.0.0",
-            "pico-args:0.4.1",
-            "proc-macro2:1.0.27",
-            "profile:0.0.0",
-            "quote:1.0.9",
-            "rayon:1.5.1",
-            "regex:1.5.4",
-            "rowan:0.14.1",
-            "salsa:0.17.0-pre.1",
-            "serde:1.0.126",
-            "serde_json:1.0.64",
-            "serde_path_to_error:0.1.4",
-            "smallvec:1.6.1",
-            "smol_str:0.1.21",
-            "stdx:0.0.0",
-            "tempfile:3.2.0",
-            "text_edit:0.0.0",
-            "thiserror:1.0.24",
-            "threadpool:1.8.1",
-            "tikv-jemallocator:0.4.1",
-            "tree-sitter:0.19.5",
-            "tree-sitter-erlang:0.1.0",
-            "vfs:0.0.0",
-            "vfs-notify:0.0.0",
-            "walkdir:2.3.2",
-            "xshell:0.1.12",
-            "xtask:0.1.0",
-        ]
-        .iter()
-        .map(|spec| spec.parse().unwrap()),
-    );
-
-    let krates: Krates = builder.build(cmd, |pkg: cm::Package| {
-        println!("Crate {} was filtered out", pkg.id);
-    })?;
-
-    // clean up the labels for the nodes
-    let graph = krates.graph().map(
-        |_, p| {
-            let name = match p {
-                Node::Krate { krate, .. } => krate.name.to_string(),
-                Node::Feature { name, .. } => name.to_string(),
-            };
-            Some(GraphNode { name })
-        },
-        |_, e| e,
-    );
-
-    // Print a dot graph of the entire crate graph
-    let contents = format!(
-        "{:?}",
-        petgraph::dot::Dot::with_config(&graph, &[Config::EdgeNoLabel])
-    );
-
-    Ok(contents)
-}
-
-struct GraphNode {
-    name: String,
-}
-
-impl fmt::Display for GraphNode {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.name)
-    }
-}
-
-impl fmt::Debug for GraphNode {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.name)
-    }
 }
